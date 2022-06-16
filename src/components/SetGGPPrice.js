3ce9cd9bb8602687eb648660864272f4f58a6a3a
CarterLynch <carter@tiogaventures.com>
2022-06-16 09:37:01 -0700

    Comments

--- src/components/SetGGPPrice.js
+++ src/components/SetGGPPrice.js
@@ -11,10 +11,12 @@ function SetGGPPrice() {
     const oracleInterface = new utils.Interface(oracleABI.abi)
     const oracleContract = new Contract(contractAddresses["Oracle"], oracleInterface)
 
+    // Calling smart contract function setGGPPrice()
     const { state, send } = useContractFunction(oracleContract, 'setGGPPrice',{ transactionName: 'SetPrice'})
     const { status } = state
 
     const setPrice = () => {
+      // Args are in send(), (price,timestamp)
       void send(ethers.utils.parseEther("3331"),9999)
     }
 
