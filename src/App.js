50cc183b8ae0ee035dfc70001fcd316e9e656228
CarterLynch <carter@tiogaventures.com>
2022-06-17 13:04:49 -0700

    Re stylinh

--- src/App.js
+++ src/App.js
@@ -7,34 +7,18 @@ import Oracle from "./components/Oracle";
 import SetGGPPrice from "./components/SetGGPPrice";
 import MinipoolManager from "./components/MinipoolManager";
 
-
-import {
-  MDBCard,
-  MDBCardBody,
-  MDBCardFooter,
-  MDBCardHeader,
-  MDBCardImage,
-  MDBCardOverlay,
-  MDBCardSubTitle,
-  MDBCardTitle,
-  MDBCardText,
-  MDBCardGroup,
-  MDBCardLink,
-} from 'mdb-react-ui-kit';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="left">
-          <TestAccounts/>
-        </div>
-        <div className="right">
           <VaultBalances />
           <TokenggAvax/>
           <Oracle/>
           <MinipoolManager/>
-        </div>
       </header>
+      <body className="App-body">
+        <TestAccounts/>
+      </body>
     </div>
   );
 }
